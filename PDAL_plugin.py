bbf7eb7f6361634754f9a726f9b3ed9092793ca2
fleaxiao <fleaxiao@gmail.com>
2024-06-12 21:56:32 +0200

    UI

--- PDAL_plugin.py
+++ PDAL_plugin.py
@@ -18,102 +18,35 @@ class PDAL_plugin(pcbnew.ActionPlugin):
         self.show_toolbar_button = True
         self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
 
-    def record_loop(self):
-        global FLAG_RECORD
-        global RECORD_DESIGN
-
-        FLAG_RECORD = True
-        prev_module_status = pd.DataFrame()
-        prev_track_status = pd.DataFrame()
-        prev_via_status = pd.DataFrame()
-
-        while FLAG_RECORD:
-            time.sleep(0.1)
-            new_module_status, new_track_status, new_via_status = pcb_record()
-
-            if not new_module_status.equals(prev_module_status) or not new_track_status.equals(prev_track_status) or not new_via_status.equals(prev_via_status):
-                if not is_module_selected() and not is_track_selected():
-                    data_dict = new_module_status.to_dict('records')
-
-                    for record in data_dict:
-                        module_ref = record.get('Module Reference')
-
-                        if module_ref not in RECORD_DESIGN['Module']:
-                            RECORD_DESIGN['Module'][module_ref] = {'Position X': [], 'Position Y': [], 'Angle': []}
-
-                        pos_x = record.get('Position X')
-                        pos_y = record.get('Position Y')
-                        angle = record.get('Angle')
-
-                        RECORD_DESIGN['Module'][module_ref]['Position X'].append(pos_x)
-                        RECORD_DESIGN['Module'][module_ref]['Position Y'].append(pos_y)
-                        RECORD_DESIGN['Module'][module_ref]['Angle'].append(angle)
-                    
-                    RECORD_DESIGN['Track']['Net'].append(new_track_status['Net'].tolist())
-                    RECORD_DESIGN['Track']['Start X'].append(new_track_status['Start X'].tolist())
-                    RECORD_DESIGN['Track']['Start Y'].append(new_track_status['Start Y'].tolist())
-                    RECORD_DESIGN['Track']['End X'].append(new_track_status['End X'].tolist())
-                    RECORD_DESIGN['Track']['End Y'].append(new_track_status['End Y'].tolist())
-                    RECORD_DESIGN['Track']['Width'].append(new_track_status['Width'].tolist())
-                    RECORD_DESIGN['Track']['Layer'].append(new_track_status['Layer'].tolist())
-
-                    RECORD_DESIGN['Via']['Net'].append(new_via_status['Net'].tolist())
-                    RECORD_DESIGN['Via']['Position X'].append(new_via_status['Position X'].tolist())
-                    RECORD_DESIGN['Via']['Position Y'].append(new_via_status['Position Y'].tolist())
-                    RECORD_DESIGN['Via']['Diameter'].append(new_via_status['Diameter'].tolist())
-
-                    prev_module_status = new_module_status
-                    prev_track_status = new_track_status
-                    prev_via_status = new_via_status
-    
     def initialization(self, event):
         pcb_init()
-
         self.text2.SetLabel('Environment is ready.')
 
-    def start_record(self, event):
+    def on_slider_change(self, event):
+        slider_value = self.slider.GetValue()
+        self.slider_value.SetValue(str(slider_value))
+    
+    def load_action(self, event):
         global RECORD_DESIGN
-
-        random_placement()
-        RECORD_DESIGN = {'Module':{},
-                         'Track':{'Net':[], 'Start X': [], 'Start Y': [], 'End X': [], 'End Y': [], 'Width': [], 'Layer': []},
-                         'Via':{'Net':[], 'Position X': [], 'Position Y': [], 'Diameter': []}}
-        thread = threading.Thread(target = self.record_loop).start()
-
-        self.text2.SetLabel('Recording...')
-
-
-    def end_record(self, event):
-        global FLAG_RECORD
-        global RECORD_DESIGN
-
-        FLAG_RECORD = False
-        record_data = json.dumps(RECORD_DESIGN, indent=4)
-
-        now = datetime.now()
-        time_string = now.strftime("%Y%m%d_%H%M%S")
-        # filename = 'pcb_record_' + time_string + '.json' #? Add the current time to the filename
-        filename = 'pcb_record' + '.json' #? Keep the filename unchanged
-        with open(filename, 'w') as file:
-            file.write(record_data)
-        
         RECORD_DESIGN = {}
+
+        board = pcbnew.GetBoard()
+        work_dir, in_pcb_file = os.path.split(board.GetFileName())
+        dlg = wx.FileDialog(self.frame, "Choose a record file", work_dir, '', "JSON files (*.json)|*.json", wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK: 
+            filepath = dlg.GetPath() 
+            with open(filepath, 'r') as file:
+                data = json.load(file)
+            RECORD_DESIGN = data
+            self.text2.SetLabel('File is loaded.')
+            self.text3.SetValue(os.path.basename(filepath))
+        dlg.Destroy() 
+
+    def play_action(self, event):
 
         self.text2.SetLabel('Reocrd is finished!')
 
-    def abandon_record(self, event):
-        global FLAG_RECORD
-        global RECORD_DESIGN
-
-        FLAG_RECORD = False
-        RECORD_DESIGN = {}
-        # board: pcbnew.BOARD = pcbnew.GetBoard()
-        # for item in list(board.GetDrawings()):
-        #     if isinstance(item, pcbnew.PCB_SHAPE) and item.GetLayer() == pcbnew.Edge_Cuts:
-        #         board.Remove(item)
-        # for module in board.GetFootprints():
-        #     board.RemoveNative(module)
-        pcbnew.Refresh()
+    def label_action(self, event):
 
         self.text2.SetLabel('Reocrd is abandoned!')
         
@@ -123,50 +56,72 @@ class PDAL_plugin(pcbnew.ActionPlugin):
         os.chdir(work_dir) # Change the working directory to the directory of the PCB file
 
         self.frame = wx.Frame(None, -1, style=wx.STAY_ON_TOP)
-        self.frame.SetTitle("Design PCB Action Recorder")
-        self.frame.SetSize(0,0,500,170)
+        self.frame.SetTitle("Design PCB Action Labeler")
+        self.frame.SetSize(0,0,500,200)
         self.frame.SetBackgroundColour(wx.WHITE)
 
         displaySize = wx.DisplaySize()
         self.frame.SetPosition((10, displaySize[1] - self.frame.GetSize()[1]-100))
 
-        # Set the icon
+        # Set icon
         icon = wx.Icon(os.path.join(os.path.dirname(__file__), 'icon.ico'), wx.BITMAP_TYPE_ICO)
         self.frame.SetIcon(icon)
 
-        # Set the image
+        # Set image
         image = wx.Image(os.path.join(os.path.dirname(__file__), 'tue.png'), wx.BITMAP_TYPE_ANY)
         image = image.Scale(100, 100, wx.IMAGE_QUALITY_HIGH)
         bitmap = wx.Bitmap(image)
         wx.StaticBitmap(self.frame, -1, bitmap, (50, 20))
 
-        # Create a StaticBitmap with the bitmap
-        self.text1 = wx.StaticText(self.frame, label = 'PCB Design Action Recorder   Copyright \u00A9 fleaxiao', pos = (130,150))
+        # Create text
+        self.text1 = wx.StaticText(self.frame, label = 'PCB Design Action Labeler   Copyright \u00A9 fleaxiao', pos = (130,180))
         self.text1.SetFont(wx.Font(6, wx.DECORATIVE, wx.NORMAL, wx.BOLD))
         self.text1.SetForegroundColour(wx.LIGHT_GREY)
 
-        self.text2 = wx.StaticText(self.frame, label = 'Record is ready', pos = (240,125), size = (140,10), style=wx.ALIGN_CENTER)
+        self.text2 = wx.StaticText(self.frame, label = 'PDAL is ready', pos = (50,155), size = (100,10), style=wx.ALIGN_CENTER)
         self.text2.SetFont(wx.Font(7, wx.DECORATIVE, wx.NORMAL, wx.BOLD))
         self.text2.SetForegroundColour(wx.RED)
-        self.text2.SetWindowStyle(wx.ALIGN_CENTER) 
+        self.text2.SetWindowStyle(wx.ALIGN_CENTER)
 
-        # Create first button
-        self.button1 = wx.Button(self.frame, label = 'Initialization', pos = (180,15), size=(260, 30))
+        self.text3 = wx.TextCtrl(self.frame, pos = (180,50), size = (165,25), style = wx.TE_READONLY)
+
+        self.text4 = wx.TextCtrl(self.frame, value="ID", pos = (180,110), size = (20,25))
+
+        self.text5 = wx.TextCtrl(self.frame, value="Instructions", pos = (205,110), size = (135,25))
+
+        # Create slider
+        self.slider = wx.Slider(self.frame, value=10, minValue=10, maxValue=50, pos=(220,80), size=(110,25), style=wx.SL_HORIZONTAL)
+        self.min_value_text = wx.StaticText(self.frame, label="10", pos=(210, 90))
+        self.min_value_text.SetFont(wx.Font(7, wx.DECORATIVE, wx.NORMAL, wx.BOLD))
+        self.max_value_text = wx.StaticText(self.frame, label="50", pos=(330, 90))
+        self.max_value_text.SetFont(wx.Font(7, wx.DECORATIVE, wx.NORMAL, wx.BOLD))
+        self.slider_value = wx.TextCtrl(self.frame, value = str(self.slider.GetValue()), pos=(180,80), size=(20,25), style=wx.TE_READONLY)
+        self.slider.Bind(wx.EVT_SLIDER, self.on_slider_change)
+        self.slider_value.SetWindowStyle(wx.ALIGN_CENTER)
+
+        # Create button
+        self.button1 = wx.Button(self.frame, label = 'Initialization', pos = (180,15), size=(260, 25))
         self.button1.Bind(wx.EVT_BUTTON, self.initialization)
 
-        self.line1 = wx.StaticLine(self.frame, pos=(185, 50), size=(240,1), style=wx.LI_HORIZONTAL)
+        self.button2 = wx.Button(self.frame, label = 'Load', pos = (350,50), size=(90, 25))
+        self.button2.Bind(wx.EVT_BUTTON, self.load_action)
 
-        self.button2 = wx.Button(self.frame, label = 'Start Record', pos = (180,55), size=(260, 30))
-        self.button2.Bind(wx.EVT_BUTTON, self.start_record)
+        self.button3 = wx.Button(self.frame, label = 'Play', pos = (350,80), size=(45, 25))
+        self.button3.Bind(wx.EVT_BUTTON, self.play_action)
 
-        self.button3 = wx.Button(self.frame, label = 'End Record', pos = (180,90), size=(130, 30))
-        self.button3.Bind(wx.EVT_BUTTON, self.end_record)
+        self.button4 = wx.Button(self.frame, label = 'Replay', pos = (395,80), size=(45, 25))
+        self.button4.Bind(wx.EVT_BUTTON, self.play_action)
 
-        self.button4 = wx.Button(self.frame, label = 'Abandon Record', pos = (310,90), size=(130, 30))
-        self.button4.Bind(wx.EVT_BUTTON, self.abandon_record)
+        self.button5 = wx.Button(self.frame, label = 'Label', pos = (350,110), size=(90, 25))
+        self.button5.Bind(wx.EVT_BUTTON, self.label_action)
 
-        self.line2 = wx.StaticLine(self.frame, pos=(40, 142), size=(120,1), style=wx.LI_HORIZONTAL)
-        self.line3 = wx.StaticLine(self.frame, pos=(185, 142), size=(240,1), style=wx.LI_HORIZONTAL)
+        self.button6 = wx.Button(self.frame, label = 'Output label file', pos = (180,140), size=(260, 25))
+        self.button6.Bind(wx.EVT_BUTTON, self.initialization)
+
+        # Create line
+        self.line1 = wx.StaticLine(self.frame, pos=(185, 45), size=(240,1), style=wx.LI_HORIZONTAL)
+        self.line2 = wx.StaticLine(self.frame, pos=(40, 172), size=(120,1), style=wx.LI_HORIZONTAL)
+        self.line3 = wx.StaticLine(self.frame, pos=(185, 172), size=(240,1), style=wx.LI_HORIZONTAL)
 
         self.frame.Show()
 
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# PCB Design Action Recorder
+# PCB Design Action Labeler
 
-PCB Design Action Recorder (PDAR) is a plugin specifically designed for KiCad that allows use to record their actions when designing PCB layout and export a `.csv` file of each step.
+PCB Design Action Labeler (PDAL) is a plugin specifically designed for KiCad that allows user to label their PCB design action to specific task number and language instructions.
 
-**Note:** This plugin has only been tested with KiCad 7.0. It's likely incompatible with later versions.
+PDAL and PCB Design Action Recorder (PDAR, https://github.com/fleaxiao/PDAR_plugin.git) are series plugins, which are recommend to use together, to fully log the human's thinking during PCB design process.
+
+ **Note:** This plugin has only been tested with KiCad 7.0. It's likely incompatible with later versions.
 
 ## Setup
 Launch KiCad and navigate to the folder where plugins are stored.
@@ -13,21 +15,24 @@ Open a terminal and change directory to the plugin folder:
 cd [path-to-plugins-folder]
 ```
 
-Clone the DAPE-plugin repository:
+Clone the PDAL_plugin repository:
 ```bash
-git clone https://github.com/fleaxiao/DP_plugin.git
+git clone https://github.com/fleaxiao/PDAL_plugin.git
 ```
 
 Refresh your plugins in KiCad:
 - Go to: `Kicad -> Tools -> External Plugins -> Refresh Plugins`
 
-The PDAC plugin should now be visible in the menu of KiCad.
+The PDAL plugin should now be visible in the menu of KiCad.
 
 ## Usage
 
+1. Open both schematic file and PCB layout file. Make sure the PCB layout is empty.
 
-1. Open both schematic file and PCB layout file. Make sure the PCB layout is empty
-1. `Initilization:`  The components from the schematic will be imported to the PCB layout design environment. 
-1. ``Start Record:`` The components of a Buck conveter will be intialized with random positions, and these components will be overlapped. The size of PCB board is fixed to `60mm x 60mm`. 
-2. ``End Record:`` After manual design (select, move and rotate of footprint), a `.json` file containg a state squence of footprints, tracks, and vias will be exported.
-3. ``Abandon Record:`` The current design will be delected.
+2. `Initilization:` The components from the schematic will be imported to the PCB layout design environment. 
+
+3. `Load:` The `.json` record file should be selected. 
+
+4. `Play:` The recorded PCB design action will be played. The number of action step is controlled by the bar.
+
+5. `Label:` After typing the specific task number and language instructions, a new labeled '.json' file will be exported.
